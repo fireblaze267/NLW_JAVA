fabd2ec9ea36deb23f115d5634f6728a2b7bd5f1
Vinicius Gama Sá <31103259+fireblaze267@users.noreply.github.com>
2025-02-20 19:56:41 -0300

    first commit

---
+++ src/main/java/br/com/nlw/events/dto/SubscriptionRankingByUser.java
@@ -0,0 +1,5 @@
+package br.com.nlw.events.dto;
+
+public record SubscriptionRankingByUser(SubscriptionRakingItem item, Integer position) {
+
+}
---
+++ src/main/java/br/com/nlw/events/exception/UserIndicadotrNotFoundException.java
@@ -0,0 +1,7 @@
+package br.com.nlw.events.exception;
+
+public class UserIndicadotrNotFoundException extends RuntimeException {
+    public UserIndicadotrNotFoundException(String msg){
+        super(msg);
+    }
+}
